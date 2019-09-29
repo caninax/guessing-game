1f6ba112c0afd6a135a876c9fc506208f57993e1
caninax <cani_nax@yahoo.com>
2019-09-29 17:57:43 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return Math.ceil((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.max = Math.ceil((this.max + this.min) / 2);
     }
 
     greater() {
-
+        this.min = Math.ceil((this.max + this.min) / 2);
     }
 }
 
